60e1f6d9216b75c4abff6b11388a7a67cb95b784
lingtalfi <lingtalfi@gmail.com>
2019-09-23 15:05:40 +0200

    update Routine update.

--- Handler/LightEndRoutineDebuggerHandler.php
+++ Handler/LightEndRoutineDebuggerHandler.php
@@ -4,6 +4,8 @@
 namespace Ling\Light_EndRoutine_Debugger\Handler;
 
 use Ling\ArrayToString\ArrayToStringTool;
+use Ling\BabyYaml\BabyYamlUtil;
+use Ling\Bat\BDotTool;
 use Ling\Light_EndRoutine\Handler\ContainerAwareLightEndRoutineHandler;
 use Ling\Light_Logger\LightLoggerService;
 
@@ -17,6 +19,20 @@ class LightEndRoutineDebuggerHandler extends ContainerAwareLightEndRoutineHandle
     /**
      * This property holds the options for this instance.
      * Options for this class.
+     *
+     * The  available options are:
+     *
+     * - showSession: bool=false, whether to add the session variables into the debug output
+     * - sessionVars: array|null=null, what session vars to show in particular. If null, all session vars
+     *                  are shown.
+     * - path: string=null. The path where to write the debug output. If null,
+     *          the @page(logger) service will be used with the method "debug".
+     *
+     *
+     *
+     *
+     *
+     *
      *
      *
      * @var array
@@ -41,23 +57,45 @@ class LightEndRoutineDebuggerHandler extends ContainerAwareLightEndRoutineHandle
 
 
         $showSession = $this->options['showSession'] ?? false;
+        $sessionVars = $this->options['sessionVars'] ?? null;
+        $path = $this->options['path'] ?? null;
+
+
         $array = [];
 
         if (true === $showSession) {
             if (session_status() == PHP_SESSION_NONE) {
                 session_start();
             }
-            $array['SESSION'] = $_SESSION;
+
+            $sessionArray = [];
+            if (null === $sessionVars) {
+                $sessionArray = $_SESSION;
+            } else {
+                if (false === is_array($sessionVars)) {
+                    $sessionVars = [$sessionVars];
+                }
+                foreach ($sessionVars as $var) {
+                    $value = BDotTool::getDotValue($var, $_SESSION);
+                    $sessionArray[$var] = $value;
+                }
+            }
+            $array['SESSION'] = $sessionArray;
         }
 
 
-        /**
-         * @var $logger LightLoggerService
-         */
-        $logger = $this->container->get("logger");
-        $logger->debug(ArrayToStringTool::toPhpArray([
-            "---LightEndRoutineDebuggerHandler---" => $array,
-        ]));
+        if (null === $path) {
+
+            /**
+             * @var $logger LightLoggerService
+             */
+            $logger = $this->container->get("logger");
+            $logger->debug(ArrayToStringTool::toPhpArray([
+                "---LightEndRoutineDebuggerHandler---" => $array,
+            ]));
+        } else {
+            BabyYamlUtil::writeFile($array, $path);
+        }
     }
 
 
